514aef0e729f13ea8ebc80e2adb965334982c0fe
JoyboyBrian <brian@nexa4ai.com>
2024-10-10 23:18:00 +0000

    clean `input_generator.py`

--- optimum_benchmark/generators/input_generator.py
+++ optimum_benchmark/generators/input_generator.py
@@ -7,16 +7,8 @@ class InputGenerator:
     task_generator: TaskGenerator
 
     def __init__(self, task: str, input_shapes: Dict[str, int], model_shapes: Dict[str, int]) -> None:
-        if task in TASKS_TO_GENERATORS:
-            shapes = {**input_shapes, **model_shapes}
-            self.task_generator = TASKS_TO_GENERATORS[task](shapes=shapes, with_labels=False)
-        else:
-            raise NotImplementedError(
-                f"Task {task} is not supported. "
-                f"Available tasks: {list(TASKS_TO_GENERATORS.keys())}. "
-                "If you want to add support for this task, "
-                "please submit a PR or a feature request to optimum-benchmark. "
-            )
+        shapes = {**input_shapes, **model_shapes}
+        self.task_generator = TASKS_TO_GENERATORS[task](shapes=shapes, with_labels=False)
 
     def __call__(self) -> Dict[str, Any]:
         task_input = self.task_generator()
